c0cc46a4dd184e065be98d305fdc283866e6908f
vivek rajendra joge <70814190+BaBu-eng@users.noreply.github.com>
2022-10-06 11:38:51 +0530

    Add files via upload

---
+++ Login Form/main.js
@@ -0,0 +1,25 @@
+console.log("Hii!")
+        function MySubmit(){
+    window.location.href = "./admin.html"
+}
+
+
+
+// *************admin page*******************
+function one() {
+    document.getElementById("FirstContener").innerHTML = "<h3>Worker Name is vivek</h3>";
+}
+
+
+// if(one()==true){
+//     let first = document.getElementById("first");
+//     first.innerHTML = "<h4>Hear rate monitaring</h4>"; 
+// }
+ 
+
+// let secnd = document.getElementsById("secnd");
+// secnd.innerHTML = "<h4>Location</h4>";
+
+
+// let thid = document.getElementById("third");
+// thid.innerHTML = "<h4>Tempature</h4>";
